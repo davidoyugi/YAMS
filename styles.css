4a261340ef86673f82f64914782e8b5024e06f2a
davidoyugi <87173050+davidoyugi@users.noreply.github.com>
2021-07-25 21:21:47 +0300

    Add files via upload

---
+++ styles.css
@@ -0,0 +1,47 @@
+
+
+function akanNames() {
+
+    
+    // getting the form;
+
+    let inputMonth = Number(document.getElementById("month").value);
+    let inputDay = Number(document.getElementById("day").value);
+    let inputYear = Number(document.getElementById("year").value);
+    let gender = document.querySelector('input[name="gender"]:checked');
+    if (inputMonth <= 0 || inputMonth > 12) {
+        alert("Please Enter a Valid Month")
+    } else if (inputDay <= 0 || inputDay > 31) {
+             alert("enter valid date");
+    } else if (inputYear === ""){
+        alert("Please Fill in the year")
+    } else if(gender == null){
+        alert("Please enter your gender")
+    } else if (inputYear >new Date().getFullYear()){
+        alert("Enter valid year!!")
+    } 
+    
+    // work
+
+var dob = inputMonth + ", " + inputDay+ ", "+inputYear;
+var then = new Date(dob);
+var theDay = then.getDay();
+var akanMale = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+var akanFemale = ['Akosua', 'Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+var weekDay =['Sunday','Monday','Tuesdays','Wednesday','Thursday','Friday','Saturday'];
+if(gender.value=="male"){
+    alert("Your akan name is: "+akanMale[theDay] +" Born on :"+weekDay[theDay]);
+}
+if(gender.value == 'female'){
+    alert("Your akan name is: "+akanFemale[theDay] +" Born on :"+weekDay[theDay]);
+}
+}
+    
+   
+
+
+
+
+
+
+
